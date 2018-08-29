a324895526888c233261a5ea6466d81b4694b210
jcmm <jcmmelton@gmail.com>
2018-08-28 23:55:49 -0400

    stage one done son

--- src/main.rs
+++ src/main.rs
@@ -2,126 +2,54 @@ extern crate image;
 
 use image::{ImageBuffer, Pixel, Rgb};
 use std::f32;
-use std::ops::{Add, Sub, Mul, Div};
 
-struct Vec3 {
-    e: [f32; 3]
-}
+mod vec3;
+use vec3::*;
 
-impl Vec3 {
-
-    fn new(e0: f32, e1: f32, e2: f32) -> Self {
-        Vec3{
-            e: [e0, e1, e2]
-        }
-    }
-
-    fn x(&self) -> f32 {
-        self.e[0]
-    }
-
-    fn y(&self) -> f32 {
-        self.e[1]
-    }
-
-    fn z(&self) -> f32 {
-        self.e[2]
-    }
-
-    fn r(&self) -> f32 {
-        self.e[0]
-    }
-
-    fn g(&self) -> f32 {
-        self.e[1]
-    }
-
-    fn b(&self) -> f32 {
-        self.e[2]
-    }
-
-    fn length(&self) -> f32 {
-        f32::sqrt(f32::powf(e[0], 2.0) + f32::powf(e[1], 2.0) + f32::powf(e[2], 2.0))
-    }
-
-    fn squared_length(&self) -> f32 {
-        f32::powf(e[0], 2.0) + f32::powf(e[1], 2.0) + f32::powf(e[2], 2.0)
-    }
-
-    fn make_unit_vector(&mut self) {
-        let k: f32 = 1.0 / self.length();
-        self.e[0] *= k;
-        self.e[0] *= k;
-        self.e[0] *= k;
-    }
-
-}
-
-impl Add for Vec3 {
-    type Output = Vec3;
-
-    fn add(self, other: Vec3) -> Vec3 {
-        Vec3::new(
-            self.e[0] + other.e[0],
-            self.e[1] + other.e[1],
-            self.e[2] + other.e[2]
-        )
-    }
-}
-
-impl Sub for Vec3 {
-    type Output = Vec3;
-
-    fn sub(self, other: Vec3) -> Vec3 {
-        Vec3::new(
-            self.e[0] - other.e[0],
-            self.e[1] - other.e[1],
-            self.e[2] - other.e[2]
-        )
-    }
-}
-
-impl Mul for Vec3 {
-    type Output = Vec3;
-
-    fn mul(self, other: Vec3) -> Vec3 {
-        Vec3::new(
-            self.e[0] * other.e[0],
-            self.e[1] * other.e[1],
-            self.e[2] * other.e[2]
-        )
-    }
-}
-
-impl Div for Vec3 {
-    type Output = Vec3;
-
-    fn div(self, other: Vec3) -> Vec3 {
-        Vec3::new(
-            self.e[0] / other.e[0],
-            self.e[1] / other.e[1],
-            self.e[2] / other.e[2]
-        )
-    }
-}
-
+#[derive(Debug, PartialEq)]
 struct Ray {
     a: Vec3,
     b: Vec3
 }
 
 impl Ray {
+
+    fn new(a: Vec3, b: Vec3) -> Self {
+        Ray {
+            a,
+            b
+        }
+    }
+
     fn origin(&self) -> Vec3 {
-        self.a
+        Vec3::from_vec3(&self.a)
     }
 
     fn direction(&self) -> Vec3 {
-        self.b
+        Vec3::from_vec3(&self.b)
     }
 
     fn point_at_parameter(&self, t: f32) -> Vec3 {
-        self.a + t*self.b
+        self.origin() + t*self.direction()
     }
+}
+
+fn hit_sphere(center: &Vec3, radius: &f32, r: &Ray) -> bool {
+    let oc: Vec3 = r.origin() - center.copy();
+    let a: f32 = Vec3::dot(&r.direction(), &r.direction());
+    let b: f32 = 2.0 * Vec3::dot(&oc, &r.direction());
+    let c: f32 = Vec3::dot(&oc, &oc) - radius*radius;
+    let discriminant: f32 = f32::powf(b, 2.0) - (4.0*a*c);
+    discriminant > 0.0
+}
+
+fn color(ray: Ray) -> Vec3 {
+    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), &0.5, &ray) {
+        return Vec3::new(1.0, 0.0, 0.0);
+    }
+    let unit_direction: Vec3 = Vec3::unit_vector(ray.direction());
+    let t: f32 = 0.5 * (unit_direction.y() + 1.0);
+    (1.0-t)*Vec3::new(1.0, 1.0, 1.0) + t*Vec3::new(0.5, 0.7, 1.0)
 }
 
 fn main() {
@@ -130,14 +58,23 @@ fn main() {
     let fx: f32 = nx as f32;
     let fy: f32 = ny as f32;
     let mut img = ImageBuffer::new(nx, ny);
+    let lower_left_corner: Vec3 = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal: Vec3    = Vec3::new(4.0, 0.0, 0.0);
+    let vertical: Vec3      = Vec3::new(0.0, 2.0, 0.0);
+    let origin: Vec3        = Vec3::new(0.0, 0.0, 0.0);
     for j in 0..ny-1 {
         for i in 0..nx {
-            let rj:u32 = ny - j;
-            let r: u8 = (255.99 * (i  as f32) / fx) as u8;
-            let g: u8 = (255.99 * (rj as f32) / fy) as u8;
-            let b: u8 = (255.99 * 0.2) as u8;
-            let col: Vector3<u8> = Vector3::new(r, g, b);
-            let pixel = Rgb::from_channels(col[0], col[1], col[2], 0);
+            let u: f32 = (i as f32)/fx;
+            let v: f32 = ((ny-j) as f32)/fy;
+            let r: Ray = Ray::new(
+                origin.copy(),
+                lower_left_corner.copy() + (u*horizontal.copy()) + (v*vertical.copy())
+            );
+            let col: Vec3 = color(r);
+            let r: u8 = (255.99 * col.r()) as u8;
+            let g: u8 = (255.99 * col.g()) as u8;
+            let b: u8 = (255.99 * col.b()) as u8;
+            let pixel = Rgb::from_channels(r, g, b, 0);
             img.put_pixel(i, j, pixel);
         }
     }
--- src/vec3.rs
+++ src/vec3.rs
@@ -2,23 +2,91 @@ use std::ops::{Neg, Div, DivAssign, Sub, SubAssign, Add, AddAssign, Mul, MulAssi
 use std::f32;
 
 #[derive(Debug, PartialEq)]
-struct Vec3 {
+pub struct Vec3 {
     e: [f32; 3]
 }
 
 impl Vec3 {
-    fn new(x: f32, y: f32, z: f32) -> Self {
-        Vec3{
+
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Vec3 {
             e: [x, y, z]
         }
     }
-    fn length(&self) -> f32 {
+
+    pub fn from_u8(x: u8, y: u8, z: u8) -> Self {
+        Vec3 {
+            e: [x as f32, y as f32, z as f32]
+        }
+    }
+
+    pub fn from_vec3(v: &Vec3) -> Self {
+        Vec3 {
+            e: [
+                v.e[0],
+                v.e[1],
+                v.e[2]
+            ]
+        }
+    }
+
+    pub fn x(&self) -> f32 {
+        self.e[0]
+    }
+
+    pub fn y(&self) -> f32 {
+        self.e[1]
+    }
+
+    pub fn z(&self) -> f32 {
+        self.e[2]
+    }
+
+    pub fn r(&self) -> f32 {
+        self.e[0]
+    }
+
+    pub fn g(&self) -> f32 {
+        self.e[1]
+    }
+
+    pub fn b(&self) -> f32 {
+        self.e[2]
+    }
+
+    pub fn x_u8(&self) -> u8 {
+        self.e[0] as u8
+    }
+
+    pub fn y_u8(&self) -> u8 {
+        self.e[1] as u8
+    }
+
+    pub fn z_u8(&self) -> u8 {
+        self.e[2] as u8
+    }
+
+    pub fn r_u8(&self) -> u8 {
+        self.e[0] as u8
+    }
+
+    pub fn g_u8(&self) -> u8 {
+        self.e[1] as u8
+    }
+
+    pub fn b_u8(&self) -> u8 {
+        self.e[2] as u8
+    }
+
+    pub fn length(&self) -> f32 {
         f32::sqrt(f32::powf(self.e[0], 2.0) + f32::powf(self.e[1], 2.0) + f32::powf(self.e[2], 2.0))
     }
-    fn squared_length(&self) -> f32 {
+
+    pub fn squared_length(&self) -> f32 {
         f32::powf(self.e[0], 2.0) + f32::powf(self.e[1], 2.0) + f32::powf(self.e[2], 2.0)
     }
-    fn cross(v1: &Vec3, v2: &Vec3) -> Vec3 {
+
+    pub fn cross(v1: &Vec3, v2: &Vec3) -> Vec3 {
         Vec3{
             e: [
                 v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1],
@@ -27,13 +95,26 @@ impl Vec3 {
             ]
         }
     }
-    fn dot(v1: &Vec3, v2: &Vec3) -> f32 {
+
+    pub fn dot(v1: &Vec3, v2: &Vec3) -> f32 {
         v1.e[0]*v2.e[0] + v1.e[1]*v2.e[1] + v1.e[2]*v2.e[2]
     }
-    fn unit_vector(v: Vec3) -> Vec3 {
+
+    pub fn unit_vector(v: Vec3) -> Vec3 {
         let length: f32 = v.length();
         v/length
     }
+
+    pub fn copy(&self) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0],
+                self.e[1],
+                self.e[2]
+            ]
+        }
+    }
+
 }
 
 impl Neg for Vec3 {
@@ -217,10 +298,28 @@ impl MulAssign<f32> for Vec3 {
     }
 }
 
-fn main() {
-    let v1: Vec3 = Vec3::new(1.0, 0.5, -0.5);
-    let v2: Vec3 = Vec3::new(2.0, 1.0, 1.5);
-    let v3: Vec3 = Vec3::new(1.1, -0.9, -0.75);
-    println!("{:?}", v1/v2);
-    println!("{:?}", v3/3.2);
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+    fn mul(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                other.e[0] * self,
+                other.e[1] * self,
+                other.e[2] * self
+            ]
+        }
+    }
+}
+
+impl Sub<Vec3> for f32 {
+    type Output = Vec3;
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                other.e[0] - self,
+                other.e[1] - self,
+                other.e[2] - self
+            ]
+        }
+    }
 }
